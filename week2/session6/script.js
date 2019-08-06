9280b6a33d1d671d1facebebcfaac63cdc67722a
qoviet <nqsviet@gmail.com>
2019-08-06 09:34:25 +0700

    update session6

---
+++ week2/session6/script.js
@@ -0,0 +1,9 @@
+function square(number){
+    var result = number*number;
+    var result;
+}
+function calculator (){
+    var input = document.getElementById("input").value;
+    var result= square(input);
+    document.getElementById("result").innerHTML=result;
+}
